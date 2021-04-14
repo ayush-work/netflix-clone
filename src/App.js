acd60df63d590de0f4bc16734f237492acdeb9cd
Ayush Maurya <ayushmaurya.work@gmail.com>
2021-04-14 18:10:04 +0530

    onclick popup added

--- src/App.js
+++ src/App.js
@@ -3,24 +3,30 @@ import Row from "../src/components/Row";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
 import requests from "./requests";
+import FadeIn from "react-fade-in";
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <Banner></Banner>
-      <Row
-        isLargePoster
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-      ></Row>
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}></Row>
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}></Row>
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}></Row>
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}></Row>
-      <Row title="Documentries" fetchUrl={requests.fetchDocumentries}></Row>
-    </div>
+    <FadeIn>
+      <div className="App">
+        <Navbar></Navbar>
+        <Banner></Banner>
+        <Row
+          isLargePoster
+          title="NETFLIX ORIGINALS"
+          fetchUrl={requests.fetchNetflixOriginals}
+        ></Row>
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}></Row>
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}></Row>
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}></Row>
+        <Row
+          title="Romance Movies"
+          fetchUrl={requests.fetchRomanceMovies}
+        ></Row>
+        <Row title="Documentries" fetchUrl={requests.fetchDocumentries}></Row>
+      </div>
+    </FadeIn>
   );
 }
 
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import YouTube from "react-youtube";
 import axios from "../axios";
 import movieTrailer from "movie-trailer";
+import ScrollIntoView from "react-scroll-into-view";
+import FadeIn from "react-fade-in";
 import "../components/Row.css";
 const IMAGE_URL = `https://image.tmdb.org/t/p/w500`;
 const Row = ({ title, fetchUrl, isLargePoster }) => {
@@ -15,6 +17,7 @@ const Row = ({ title, fetchUrl, isLargePoster }) => {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
   const [clickedMovie, setClickedMovie] = useState("");
+  const popupRef = useRef();
 
   useEffect(() => {
     async function fetchMovies() {
@@ -36,38 +39,70 @@ const Row = ({ title, fetchUrl, isLargePoster }) => {
   //       .catch((err) => {});
   //   }
   // };
+
   const handleClick = (movie) => {
-    if (clickedMovie) {
+    popupRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "center",
+    });
+    if (clickedMovie !== movie) {
+      setClickedMovie(movie);
+    }
+    if (clickedMovie === movie) {
       setClickedMovie("");
-    } else setClickedMovie(movie);
+    }
   };
-  useEffect(() => {
-    console.log("clicked");
-  }, [clickedMovie]);
+
+  console.log(movies);
   return (
     <div className="row">
-      <h2>{title}</h2>
+      <h2 className="row__title">{title}</h2>
       <div className="row__posters">
         {movies.map((movie) => {
           return (
-            <>
-              <img
-                onClick={() => handleClick(movie)}
-                className={`row__poster ${isLargePoster ? `large` : ""}`}
-                src={`${IMAGE_URL}${
-                  isLargePoster ? movie.poster_path : movie.backdrop_path
-                }`}
-                alt={movie.title}
-                key={movie.id}
-              />
-            </>
+            <img
+              onClick={() => handleClick(movie)}
+              className={`row__poster ${isLargePoster ? `large` : ""}`}
+              src={`${IMAGE_URL}${
+                isLargePoster ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.title}
+              key={movie.id}
+            />
           );
         })}
       </div>
       {clickedMovie && (
-        <div className="row__poster--info">
-          {clickedMovie?.title || clickedMovie?.name}
-        </div>
+        <FadeIn>
+          <div className="row__popup" ref={popupRef}>
+            <div className="row__popup__poster">
+              <img
+                src={`${IMAGE_URL}${clickedMovie?.poster_path}`}
+                alt={clickedMovie?.title}
+              />
+              <div className="row__popup__rating">
+                {" "}
+                {clickedMovie?.adult ? "PG" : "G"}
+              </div>
+            </div>
+
+            <div className="row__popup__content">
+              <div className="row__popup__title">
+                <div>{clickedMovie?.title || clickedMovie?.name}</div>
+                <button className="row__popup__add">
+                  <i class="bx bx-plus"></i>My List
+                </button>
+              </div>
+              <h3 className="row__popup__description">
+                {clickedMovie?.overview}
+              </h3>
+              <h3 className="row__popup__vote">
+                Average Rating : {clickedMovie?.vote_average}
+              </h3>
+            </div>
+          </div>
+        </FadeIn>
       )}
       {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}></YouTube>}
     </div>
